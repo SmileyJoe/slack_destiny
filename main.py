5b6e5845a61f2ad5c6bee849a9a8dd27feb133c4
SmileyJoe <cody.strugnell@gmail.com>
2017-09-16 19:37:45 +0200

    Posting to slack

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ import requests
 import json
 from destiny import *
 from helpers import Time
+from slack import *
 
 destiny = Destiny()
 destiny.set_api_key(BUNGIE_API_KEY)
+
+users = []
+
+FORMAT_COMPACT = 1
+FORMAT_EXPANDED = 2
+
+messageFormat = FORMAT_COMPACT
 
 for key, value in DESTINY_USERS.items():
     profile = destiny.get_profile(value)
@@ -24,6 +32,27 @@ for key, value in DESTINY_USERS.items():
             maxLight = light
 
     if seconds > 0:
-        message = "{}: Time:{} Light:{}".format(key, Time.elapsed(seconds), maxLight)
+        hours = seconds/60/60
+        elapsed = Time.elapsed(seconds)
 
-        pprint(message)
+        if messageFormat == FORMAT_COMPACT:
+            message = "\n*{}:* {} hours ({})".format(key, hours, elapsed)
+        else:
+            message = "\n\n*{}*\nTime: {} hours ({})\nMax Light: {}".format(key, hours, elapsed, maxLight)
+
+        valueTuple = (key, hours, message)
+        users.append(valueTuple)
+
+users.sort(key=lambda tup: tup[1], reverse=True)
+
+slackMessage = ""
+for user in users:
+    slackMessage += user[2]
+
+slack = Slack()
+slack.token(SLACK_TOKEN)
+slack.channel(SLACK_CHANNEL)
+slack.userName("Destiny Bot")
+slack.text(slackMessage)
+slack.postMessage()
+#pprint(slackMessage)
